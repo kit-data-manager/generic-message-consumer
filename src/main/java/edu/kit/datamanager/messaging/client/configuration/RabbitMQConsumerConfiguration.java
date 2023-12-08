18e404fb62d1d970ad1cd8d3393920828b091830
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-12-08 11:08:15 +0100

    Fix #5, added null check for receiver configuration, added tasks for local artifact deployment

--- src/main/java/edu/kit/datamanager/messaging/client/configuration/RabbitMQConsumerConfiguration.java
+++ src/main/java/edu/kit/datamanager/messaging/client/configuration/RabbitMQConsumerConfiguration.java
@@ -17,10 +17,10 @@ package edu.kit.datamanager.messaging.client.configuration;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Declarables;
@@ -28,11 +28,8 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,82 +46,91 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "repo.messaging")
 @Data
-public class RabbitMQConsumerConfiguration{
+public class RabbitMQConsumerConfiguration {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumerConfiguration.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumerConfiguration.class);
 
-  /**
-   * The hostname of the RabbitMQ server. (default: localhost)
-   */
-  @Value("${repo.messaging.hostname:localhost}")
-  private String hostname;
-  /**
-   * The port of the RabbitMQ server. (default: 5672)
-   */
-  @Value("${repo.messaging.port:5672}")
-  private int port;
-  /**
-   * The username to connect of the RabbitMQ server. (default: guest)
-   */
-  @Value("${repo.messaging.username:guest}")
-  private String username;
-  /**
-   * The password to connect of the RabbitMQ server. (default: guest)
-   */
-  @Value("${repo.messaging.password:guest}")
-  private String password;
+    /**
+     * The hostname of the RabbitMQ server. (default: localhost)
+     */
+    @Value("${repo.messaging.hostname:localhost}")
+    private String hostname;
+    /**
+     * The port of the RabbitMQ server. (default: 5672)
+     */
+    @Value("${repo.messaging.port:5672}")
+    private int port;
+    /**
+     * The username to connect of the RabbitMQ server. (default: guest)
+     */
+    @Value("${repo.messaging.username:guest}")
+    private String username;
+    /**
+     * The password to connect of the RabbitMQ server. (default: guest)
+     */
+    @Value("${repo.messaging.password:guest}")
+    private String password;
 
-  /**
-   * The consumer binding used to connect to a certain exchange, establishing a
-   * queue and linking both by one or more routing keys.
-   */
-  private ConsumerBinding receiver;
+    /**
+     * The consumer binding used to connect to a certain exchange, establishing
+     * a queue and linking both by one or more routing keys.
+     */
+    private Optional<ConsumerBinding> receiver;
 
-  @Bean
-  public ConnectionFactory rabbitMQConnectionFactory(){
-    LOGGER.trace("Connecting to RabbitMQ service at host {} and port {}.", hostname, port);
-    CachingConnectionFactory factory = new CachingConnectionFactory(hostname, port);
-    factory.setUsername(username);
-    factory.setPassword(password);
-    return factory;
-  }
-
-  @Bean
-  public RabbitTemplate rabbitMQTemplate(){
-    LOGGER.trace("Get RabbitMQ template");
-    return new RabbitTemplate(rabbitMQConnectionFactory());
-  }
-
-  @Bean
-  public Queue queue(){
-    return new Queue(receiver.getQueue());
-  }
-
-  @Bean
-  public TopicExchange exchange(){
-    return new TopicExchange(receiver.getExchange());
-  }
-
-  @Bean
-  public Declarables topicBindings(){
-    LOGGER.trace("Configuring exchange {} with queue {}.", exchange(), queue());
-    List<Binding> amqpBindings = new ArrayList<>();
-
-    Declarables declarables = new Declarables();
-    LOGGER.trace("Adding queue {} to list of declarables.", queue());
-    declarables.getDeclarables().add(queue());
-    LOGGER.trace("Adding exchange {} to list of declarables.", exchange());
-    declarables.getDeclarables().add(exchange());
-    for(String routingKey : receiver.getRoutingKeys()){
-      LOGGER.trace("Adding binding via routing key {} to declarables.", routingKey);
-      amqpBindings.add(
-              BindingBuilder
-                      .bind(queue())
-                      .to(exchange())
-                      .with(routingKey)
-      );
+    @Bean
+    public ConnectionFactory rabbitMQConnectionFactory() {
+        LOGGER.trace("Connecting to RabbitMQ service at host {} and port {}.", hostname, port);
+        CachingConnectionFactory factory = new CachingConnectionFactory(hostname, port);
+        factory.setUsername(username);
+        factory.setPassword(password);
+        return factory;
     }
-    declarables.getDeclarables().addAll(amqpBindings);
-    return declarables;
-  }
+
+    @Bean
+    public RabbitTemplate rabbitMQTemplate() {
+        LOGGER.trace("Get RabbitMQ template");
+        return new RabbitTemplate(rabbitMQConnectionFactory());
+    }
+
+    @Bean
+    public Queue queue() {
+        if (receiver != null && receiver.isPresent()) {
+            return new Queue(receiver.get().getQueue());
+        }
+        return null;
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        if (receiver != null && receiver.isPresent()) {
+            return new TopicExchange(receiver.get().getExchange());
+        }
+        return null;
+    }
+
+    @Bean
+    public Declarables topicBindings() {
+        if (receiver != null && receiver.isPresent()) {
+            LOGGER.trace("Configuring exchange {} with queue {}.", exchange(), queue());
+            List<Binding> amqpBindings = new ArrayList<>();
+
+            Declarables declarables = new Declarables();
+            LOGGER.trace("Adding queue {} to list of declarables.", queue());
+            declarables.getDeclarables().add(queue());
+            LOGGER.trace("Adding exchange {} to list of declarables.", exchange());
+            declarables.getDeclarables().add(exchange());
+            for (String routingKey : receiver.get().getRoutingKeys()) {
+                LOGGER.trace("Adding binding via routing key {} to declarables.", routingKey);
+                amqpBindings.add(
+                        BindingBuilder
+                                .bind(queue())
+                                .to(exchange())
+                                .with(routingKey)
+                );
+            }
+            declarables.getDeclarables().addAll(amqpBindings);
+            return declarables;
+        }
+        return null;
+    }
 }
